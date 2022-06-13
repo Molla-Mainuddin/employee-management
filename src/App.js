aec684c0e23775fcffb560d8309d24d13fbd2d83
Molla-Mainuddin <47754230+Molla-Mainuddin@users.noreply.github.com>
2022-06-13 12:55:34 +0530

    Router added in Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Navbar from "./components/Navbar";
+import Home from "./Pages/Home/components/Home";
+import About from "./Pages/about/components/About";
+import Contact from "./Pages/contact/components/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = ()=>{
 
-  return(
+const App = () => {
+
+  return (
     <>
-      <Navbar />
+      <div className="App">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav class="h-auto flex items-center justify-between bg-teal-500 py-2 px-32">
-            <div class="flex items-center flex-shrink-0 text-white">
-                <img src='assets/employee_m-logo.jpg' alt='broken' className='h-12 w-12 rounded-full cursor-pointer' />
-                {/* <span class="font-semibold text-xl tracking-tight">Employee</span> */}
+        <nav className="h-auto flex items-center justify-between bg-teal-500 py-2 px-32">
+            <div className="flex items-center flex-shrink-0 text-white">
+                <Link to="/">
+                    <img src='assets/employee_m-logo.jpg' alt='broken' className='h-12 w-12 rounded-full cursor-pointer' />
+                </Link>
+                {/* <span className="font-semibold text-xl tracking-tight">Employee</span> */}
             </div>
-            {/* <div class="block lg:hidden">
-                <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            {/* <div className="block lg:hidden">
+                <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
                     </svg>
                 </button>
             </div> */}
-            <div class="w-full block lg:flex lg:items-center lg:w-auto">
-                <div class="text-lg lg:flex-grow">
-                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                        Docs
-                    </a>
-                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                        Examples
-                    </a>
-                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-                        Blog
-                    </a>
+            <div className="w-full block lg:flex lg:items-center lg:w-auto">
+                <div className="text-lg lg:flex-grow">
+                    <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        Home
+                    </Link>
+                    <Link to="/about" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        About
+                    </Link>
+                    <Link to="/contact" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                        Contact
+                    </Link>
                 </div>
             </div>
         </nav>
